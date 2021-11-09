a57e3864918dbb891fc8ff7e11d31d0b44d4309e
Tojo Rabemananjara <trabema1@terpmail.umd.edu>
2021-11-09 10:30:47 -0500

    after setting up netlify

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Button} from './Button';
+
+class Random extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: [9, 180, 309] };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  componentDidMount() {
+    this.applyColor();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.applyColor();
+  }
+
+  formatColor(ary) {
+    return 'rgb(' + ary.join(', ') + ')';
+  }
+
+  isLight() {
+    const rgb = this.state.color;
+    return rgb.reduce((a,b) => a+b) < 127 * 3;
+  }
+
+  applyColor() {
+    const color = this.formatColor(this.state.color);
+    document.body.style.background = color;
+  }
+
+  chooseColor() {
+    const random = [];
+    for (let i = 0; i < 3; i++) {
+      random.push(Math.floor(Math.random()*256));
+    }
+    return random;
+  }
+
+  handleClick() {
+    this.setState({
+      color: this.chooseColor()
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className={this.isLight() ? 'white' : 'black'}>
+          Your color is {this.formatColor(this.state.color)}.
+        </h1>
+        <Button
+          light={this.isLight()}
+          onClick={this.handleClick}/>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Random />, 
+  document.getElementById('app')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
